05440cf3cf15f0c045b79bcd47d4ec014407548b
kajal452 <40475954+kajal452@users.noreply.github.com>
2019-03-31 12:51:46 +0530

    Login msg library;

---
+++ loginmodule/src/main/java/com/kajalkumar/www/loginmodule/LogDebug.java
@@ -0,0 +1,11 @@
+package com.kajalkumar.www.loginmodule;
+
+import android.util.Log;
+
+public class LogDebug {
+    private static final String TAG="MSG";
+    public  static void e(String s)
+    {
+        Log.d(TAG,s);
+    }
+}
